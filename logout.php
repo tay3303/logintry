348f5d681cc0c535fb1f79b472654646b51faa05
Tay Kian Yoon <mason@Tays-MacBook-Pro.local>
2021-07-08 19:24:41 +0700

    learn new from kang and wang

--- logout.php
+++ logout.php
@@ -2,12 +2,9 @@
 
     session_start();
     session_destroy();
-    if(isset($_COOKIE['email']) and isset($_COOKIE['pass'])){
-        $email = $_COOKIE['email'];
-        $pass = $_COOKIE['pass'];
-        setcookie('email',$email, time()-1);
-        setcookie('pass',$pass, time()-1);
-    }
+    setcookie("PHPSESSID","",time()-3600,"/");
+    var_dump($_REQUEST);
+    var_dump($_SESSION);
     echo "You successfully logout. Click here to 
     <a href='index.php'> login  </a>again.";
 ?>
--- welcome.php
+++ welcome.php
@@ -1,5 +1,7 @@
 <?php 
     session_start();
+    var_dump($_REQUEST);
+    var_dump($_SESSION);
     echo "Welcome " . $_SESSION['email'];
     echo "<a href='logout.php'> logout </a>";
 ?>
